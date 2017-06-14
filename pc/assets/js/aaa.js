ef0afdddbce5d1854b57efdb41b7df666eaddf8f
heliang <heliang@duyao001.com>
2017-06-14 19:42:03 +0800

    初始化版本

---
+++ pc/assets/js/aaa.js
@@ -0,0 +1,10 @@
+/**
+ * Created by hotread on 2017/6/14.
+ */
+
+// var global="我是中国人"
+// function myAlert(){
+//     alert(global);
+// }
+// myAlert();
+// export default myAlert;
---
+++ pc/gulpfile.js
@@ -0,0 +1,48 @@
+var gulp =require('gulp');
+var sass=require('gulp-sass');
+var wrap=require('gulp-wrap');
+var rename=require('gulp-rename');
+var uglify = require('gulp-uglify');
+
+
+gulp.task('sass',function(){
+     gulp.src('sass/*.scss')
+         .pipe(sass({
+             outputStyle:'compressed'
+         }))
+         .pipe(rename({suffix:'.min'}))
+         .pipe(gulp.dest('assets/css'))
+})
+
+gulp.task('html',function(){
+    gulp.src(['_pages/*.html'])
+        .pipe(wrap({
+            src:'_layout/layout.html'
+        }))
+        .pipe(rename(function (path) {
+            var basename=path.basename;
+            path.basename=basename.replace('_','');
+        }))
+        .pipe(gulp.dest('pages/'))
+})
+
+
+gulp.task('js',function () {
+    gulp.src('assets/js/*')
+        // .pipe(uglify({mangle: true}))
+        .pipe(uglify({mangle: {toplevel: true}}))
+        .pipe(rename({suffix:'.min'}))
+        .pipe(gulp.dest('assets/minjs'))
+})
+
+
+
+
+
+gulp.task('watch',function () {
+    gulp.watch('sass/*.scss',['sass']);
+    gulp.watch('_pages/*',['html']);
+    gulp.watch('assets/js/*',['js']);
+})
+
+gulp.task('default',['watch']);
