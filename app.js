606b6b7ebcd3bc6f634814c89ef37b8859867f2c
heliang <heliang@duyao001.com>
2017-06-20 16:47:52 +0800

    添加动态数据

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'wellcome')));
 
 app.use(express.static(path.join(__dirname, 'node_modules')));
+app.use(express.static(path.join(__dirname, 'views')));
 
 app.use('/pc', express.static(path.join(__dirname, 'pc')));
 app.use('/wap', express.static(path.join(__dirname, 'wap')));
@@ -37,7 +38,8 @@ app.use(express.static(path.join(__dirname, 'data'),{
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
-  next(err);
+  // next(err);
+    res.send(err)
 });
 
 // error handler
@@ -49,6 +51,7 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+  res.send(500);
 });
 
 module.exports = app;
