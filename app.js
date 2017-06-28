29fb94e8773f9ebb2f27baa888c221279bd82c5d
heliang <heliang@duyao001.com>
2017-06-28 18:21:06 +0800

    添加免费作品页面

--- app.js
+++ app.js
@@ -6,33 +6,62 @@ var cookieParser = require('cookie-parser');
 var ejs = require('ejs');
 var bodyParser = require('body-parser');
 
+var fs=require('fs');
 var app = express();
 
 // view engine setup
-
-app.engine('html', ejs.__express);
-app.set('view engine', 'html');
-app.set('pages', path.join(__dirname, 'html'));
+//
+// app.engine('html', ejs.__express);
+// app.set('view engine', 'html');
+// app.set('pages', path.join(__dirname, 'html'));
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'wellcome')));
-
 app.use(express.static(path.join(__dirname, 'node_modules')));
-app.use(express.static(path.join(__dirname, 'views')));
-
 app.use('/pc', express.static(path.join(__dirname, 'pc')));
 app.use('/wap', express.static(path.join(__dirname, 'wap')));
-
 app.use(express.static(path.join(__dirname, 'data'),{
     setHeaders:function(res, path){
         res.setHeader('Content-Type','application/json;charset=UTF-8');
     }
 }));
+
+app.use('/rank_:type.json',function(req, res, next){
+    console.log(12213);
+    var pathName=path.join(__dirname,'data')+"/ranks.json";
+    var type=req.params.type;
+    // console.log(path);
+    // next();
+    fs.readFile(pathName,function(err,data){
+        data=data.toString();
+        if(err){
+            next(err);
+        }else{
+
+             data= JSON.parse(data).filter(function(item){
+                return  item.type==type;
+            })
+            console.log(">>>>"+JSON.stringify(data));
+             if(data.length){
+                 res.send(JSON.stringify(data[0].list))
+             }else{
+                 res.send([]);
+             }
+
+        }
+    });
+});
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
