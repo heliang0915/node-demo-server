2c0fd10b6e40c68f239f76e1e9be9430fd172fdd
heiang <598481337@qq.com>
2018-09-03 22:50:29 +0800

    修改首页

--- shop/wap/gulpfile.js
+++ shop/wap/gulpfile.js
@@ -3,14 +3,20 @@ var sass=require('gulp-sass');
 var wrap=require('gulp-wrap');
 var rename=require('gulp-rename');
 var uglify = require('gulp-uglify');
+var autoprefixer = require('gulp-autoprefixer');
 
 
 gulp.task('sass',function(){
      gulp.src(['sass/*.scss'])
          .pipe(sass({
              outputStyle:'compressed'
-         }))
-         .pipe(rename({suffix:'.min'}))
+         })).pipe(autoprefixer({
+            browsers: ['last 2 versions', 'Android >= 4.0'],
+            cascade: true, //是否美化属性值 默认：true 像这样：
+            //-webkit-transform: rotate(45deg);
+            // transform: rotate(45deg);
+            remove:true //是否去掉不必要的前缀 默认：true
+          })).pipe(rename({suffix:'.min'}))
          .pipe(gulp.dest('assets/css'))
 })
 
